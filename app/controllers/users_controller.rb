6cd27925cb7bd9d5e19f7ea3862c93e5c6268f01
Tim-Feng <fsjforever26@gmail.com>
2014-02-08 08:52:17 +0800

    Ready to Heroku

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+  before_action :require_same_user, only: [:edit, :update]
+
+  def new
+    @user = User.new  
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "You are registered."
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:user_name, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:error] = "You're not allow to do that."
+      redirect_to root_path
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 AdJudge::Application.routes.draw do
   root to: 'posts#index'
+  get '/login', to:'sessions#new'
+  post '/login', to:'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+  get '/register', to: 'users#new'
 
   resources :posts, except: [:destroy] do
     resources :comments, only: [:create]
   end
+
+  resources :users, only: [:show, :create, :edit, :update]
   
 end

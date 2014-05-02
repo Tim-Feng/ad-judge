77489d24639094c139c572045e4f22125accc2df
Tim-Feng <fsjforever26@gmail.com>
2014-05-02 10:41:37 +0800

    add facebook_helper

---
+++ app/helpers/facebook_helper.rb
@@ -0,0 +1,9 @@
+module FacebookHelper
+  def share_link(post)
+    fb_key = ENV['FACEBOOK_KEY']
+    href = <<-EOS
+https://www.facebook.com/dialog/feed?app_id=#{fb_key}&display=page&link=http://www.ad-judge.com/posts/#{post.slug}&picture=http://img.youtube.com/vi/#{post.youtube_video_id}/hqdefault.jpg&name=#{post.title}&redirect_uri=#{request.original_url}
+EOS
+    URI::encode(href)
+  end
+end

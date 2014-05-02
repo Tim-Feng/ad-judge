7daab0ef4c410df18b25b55db510981d9eb6f375
Tim-Feng <fsjforever26@gmail.com>
2014-05-02 10:56:00 +0800

    chopping off /vote from referrer

--- app/helpers/facebook_helper.rb
+++ app/helpers/facebook_helper.rb
@@ -2,7 +2,7 @@ module FacebookHelper
   def share_link(post)
     fb_key = ENV['FACEBOOK_KEY']
     href = <<-EOS
-https://www.facebook.com/dialog/feed?app_id=#{fb_key}&display=page&link=http://www.ad-judge.com/posts/#{post.slug}&picture=http://img.youtube.com/vi/#{post.youtube_video_id}/hqdefault.jpg&name=#{post.title}&redirect_uri=#{request.original_url}
+https://www.facebook.com/dialog/feed?app_id=#{fb_key}&display=page&link=http://www.ad-judge.com/posts/#{post.slug}&picture=http://img.youtube.com/vi/#{post.youtube_video_id}/hqdefault.jpg&name=#{post.title}&redirect_uri=#{request.original_url.gsub("/vote","")}
 EOS
     URI::encode(href)
   end

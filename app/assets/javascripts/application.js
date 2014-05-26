99eadffb70cab3a65f6975aaee327bfc1b851f4e
Tim-Feng <fsjforever26@gmail.com>
2014-05-26 16:48:13 +0800

    encode slug in facebook share javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,7 +21,7 @@ $(document).on('click','.social_share_column', function(){
     var slug = $(this).attr("post-id");
     FB.ui({
       method: 'share',
-      href: 'http://www.ad-judge.com/posts/' + encodeURIComponent(slug)  ,
+      href: 'http://www.ad-judge.com/posts/' + encodeURIComponent(slug),
     }, function(response){});
   });
 });
